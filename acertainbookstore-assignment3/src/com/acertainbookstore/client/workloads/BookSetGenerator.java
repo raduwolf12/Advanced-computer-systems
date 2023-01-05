e1687d8eecd4804d07e2813ba5fa91f1e049c669
Radu <rtaraburca@yahoo.com>
2023-01-05 17:31:27 +0200

    create random books + jars + classpatch update

--- acertainbookstore-assignment3/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
+++ acertainbookstore-assignment3/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
@@ -1,7 +1,16 @@
 package com.acertainbookstore.client.workloads;
 
-import java.util.Set;
+import com.github.javafaker.Faker;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.acertainbookstore.business.ImmutableStockBook;
 import com.acertainbookstore.business.StockBook;
 
 /**
@@ -10,8 +19,10 @@ import com.acertainbookstore.business.StockBook;
  */
 public class BookSetGenerator {
 
+	Faker faker;
+	
 	public BookSetGenerator() {
-		// TODO Auto-generated constructor stub
+		faker = new Faker();
 	}
 
 	/**
@@ -21,7 +32,10 @@ public class BookSetGenerator {
 	 * @return
 	 */
 	public Set<Integer> sampleFromSetOfISBNs(Set<Integer> isbns, int num) {
-		return null;
+		List<Integer> tempList = new ArrayList<>(isbns);
+		Collections.shuffle(tempList, new Random());
+		Set<Integer> result = tempList.stream().limit(num).collect(Collectors.toSet());
+		return result;
 	}
 
 	/**
@@ -31,7 +45,24 @@ public class BookSetGenerator {
 	 * @return
 	 */
 	public Set<StockBook> nextSetOfStockBooks(int num) {
-		return null;
+		Set<StockBook> resultSet = new HashSet<>();
+		for (int i = 1; i <= num; i++) {
+		    resultSet.add(generateRandomBook());
+		}
+		return resultSet;
+	}
+	public ImmutableStockBook generateRandomBook() {
+		
+	    int isbn = faker.number().numberBetween(1000000, 9999999);
+	    String title = faker.book().title();
+	    String author = faker.book().author();
+	    float price = (float) faker.number().numberBetween(1, 100);
+	    int numCopies = faker.number().numberBetween(0, 1000);
+	    long numSaleMisses = faker.number().numberBetween(0, 10000);
+	    long numTimesRated = faker.number().numberBetween(0, 1000);
+	    long totalRating = faker.number().numberBetween(0, 5000);
+	    boolean editorPick = faker.bool().bool();
+	    return new ImmutableStockBook(isbn, title, author, price, numCopies, numSaleMisses, numTimesRated, totalRating, editorPick);
 	}
 
 }
